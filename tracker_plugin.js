16313638adccd4106e2dae751a7612895f4bbb5c
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2022-04-05 10:52:12 +0200

    Manage to show students name from button press

--- tracker_plugin.js
+++ tracker_plugin.js
@@ -1,12 +1,25 @@
+var students = [];
+
 fetch('data.json')
     .then(response => response.json())
     .then(data => displayData(data));
 
+function buttonClicked(student) {
+    const targetDiv = document.getElementById("results");
+    
+    if(targetDiv.style.display === 'none') {
+        targetDiv.style.display = 'block';
+        targetDiv.innerHTML = student;
+    } else 
+        targetDiv.style.display = 'none';
+} 
+
 function displayData(obj) {
-    const container = document.querySelector("#JSONValues");
+    const container = document.getElementById('JSONValues');
     for(var i = 0; i < obj.students.length; i++) {
-        var studentNames = document.createElement('button');
-        studentNames.innerHTML = obj.students[i].name;
-        container.appendChild(studentNames);
+        students[i] = document.createElement('button');
+        students[i].insertAdjacentHTML("beforeend", obj.students[i].name);
+        students[i].addEventListener("click", buttonClicked.bind(null, obj.students[i].name));
+        container.appendChild(students[i]);
     }
 }
