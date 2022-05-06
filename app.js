e1df27487e43de17c1cdc0b5d33bd598a47d63ab
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2022-05-06 10:10:13 +0200

    Add socket.io package

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const path = require('path');
-// const { Server } = require("socket.io");
 // const displayFunctions = require('./public/tracker_plugin');
 const jsonData = require('./data.json');
 
 const app = express();
-const server = require('http').createServer(app);
-// const io = require('socket.io')(server, {cors: {origin: '*'}});
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const public = path.join(__dirname, '/public');
-const hostname = "127.0.0.1";
 const port_client = 3000;
-const port_server = 8080;
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(public, 'index.html'));
+  res.sendFile(path.join(public, '/index.html'));
 });
 
 app.use(express.static(public));
@@ -26,7 +23,7 @@ server.listen(8080, () => {
 app.listen(port_client, function() {
   console.log(`Server running at http://localhost:${port_client}/`);
 });
-/*
-io.on('connection', () => {
-  console.log(`User connected: ${socket.id}`);
-});*/
+
+io.on('connection', function(socket) {
+  console.log('A connection was made');
+})
