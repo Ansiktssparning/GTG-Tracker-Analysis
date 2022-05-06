2766a9f572ffaa09ee45378be0cd036485e00891
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2022-05-06 10:39:07 +0200

    Manage to implement TCP to announce when a user has entered the website in cmd

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const path = require('path');
-// const displayFunctions = require('./public/tracker_plugin');
-const jsonData = require('./data.json');
 
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const public = path.join(__dirname, '/public');
 const port_client = 3000;
 
+const public = path.join(__dirname, '/public');
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(public, '/index.html'));
+    res.sendFile(path.join(public, 'index.html'));
 });
 
 app.use(express.static(public));
-/*
-server.listen(8080, () => {
-  console.log('listening om *:8080');
-});*/
 
-app.listen(port_client, function() {
+server.listen(port_client, function() {
   console.log(`Server running at http://localhost:${port_client}/`);
 });
 
 io.on('connection', function(socket) {
-  console.log('A connection was made');
+    console.log('A connection was made');
 })
