997a1f2a06a33634f5b89d1d8d25ae1c4c867c34
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2022-05-17 15:26:06 +0200

    The buttons appear again!

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const port_client = 3000;
+const port_client = 3001;
 
-const public = path.join(__dirname, '/public');
+const public_folder = path.join(__dirname, '/public');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(public, 'index.html'));
+    res.sendFile(path.join(public_folder, 'index.html'));
 });
 
-app.use(express.static(public));
+app.use(express.static(public_folder));
+
+app.use(express.json());
 
 server.listen(port_client, function() {
   console.log(`Server running at http://localhost:${port_client}/`);
--- public/tracker_plugin.js
+++ public/tracker_plugin.js
@@ -12,10 +12,11 @@ socket.on('connect', function() { //Don't send until we're connected
     });
 });*/
 
-fetch('../data.json') // API som skickar nätverk requests
+fetch('http://localhost:3000/students') // API som skickar nätverk requests
     .then(response => response.json())
     .catch(error => console.log('Error:', error)) // Visar error om det fuckar upp
     .then(data => displayData(data)); // Returnerar data som JSON objekt
+
 
 function displayStudentResult(hiddenDiv) { // Togglar fram och tillbaka elevens information
     if(hiddenDiv.style.display === "none") 
@@ -28,13 +29,13 @@ function displayData(obj) {
     const container = document.getElementById('JSONValues'); // Variabel till JSONValues div:en
     var frag = document.createDocumentFragment(); // Dokument objekt som inte har en förälder
 
-    for(var i = 0; i < obj.students.length; i++) { // Körs för varje elev
+    for(var i = 0; i < obj.length; i++) { // Körs för varje elev
         var studentButton = document.createElement("button"); // Knapp element som användaren trycker på
         var studentInfo = document.createElement("div"); // Div element med info om eleven
 
         studentInfo.style.display = 'none'; // Är osynlig för användaren
         studentInfo.className = 'results'; // Tilldelas klassen 'results'
-        studentButton.textContent = obj.students[i].name; // Knapptext visar elevens namn
+        studentButton.textContent = obj[i].name; // Knapptext visar elevens namn
 
         studentButton.addEventListener("click", displayStudentResult.bind(null, studentInfo)); // Anropar funktion vid knapptryck
 
@@ -42,13 +43,13 @@ function displayData(obj) {
         var avgEyelid = document.createElement('p'); // Text för ögonlockvärde
         var timeDiagram = document.createElement('img'); // Bild av diagram
 
-        timeDiagram.src = obj.students[i].timeGraph; // Får adress till varje elevs diagram
+        timeDiagram.src = obj[i].timeGraph; // Får adress till varje elevs diagram
         timeDiagram.style.width = "600px";
         timeDiagram.style.height = "400px";
         avgGazeDir.className = "textValues"; // Tilldelas klassen 'textValues'
         avgEyelid.className = "textValues"; // Tilldelas klassen 'textValues'
-        avgGazeDir.innerHTML = `Genomsnittlig tid med ouppmärksam blickriktning: ${obj.students[i].avgGazeDirection} sekunder`;
-        avgEyelid.innerHTML = `Genomsnittlig tid med ögonlocken stängda: ${obj.students[i].avgEyelid} sekunder`;
+        avgGazeDir.innerHTML = `Genomsnittlig tid med ouppmärksam blickriktning: ${obj[i].avgGazeDirection} sekunder`;
+        avgEyelid.innerHTML = `Genomsnittlig tid med ögonlocken stängda: ${obj[i].avgEyelid} sekunder`;
 
         // Bifogar barn till dess föräldrar
         studentInfo.appendChild(avgGazeDir);  
@@ -59,47 +60,3 @@ function displayData(obj) {
     }
     container.appendChild(frag); // Bifogar fragment till container och visas på hemsidan
 }
-
-/*module.exports = {
-    displayInfoFunc: function displayData(obj) {
-        const container = document.getElementById('JSONValues'); // Variabel till JSONValues div:en
-        var frag = document.createDocumentFragment(); // Dokument objekt som inte har en förälder
-    
-        for(var i = 0; i < obj.students.length; i++) { // Körs för varje elev
-            var studentButton = document.createElement("button"); // Knapp element som användaren trycker på
-            var studentInfo = document.createElement("div"); // Div element med info om eleven
-    
-            studentInfo.style.display = 'none'; // Är osynlig för användaren
-            studentInfo.className = 'results'; // Tilldelas klassen 'results'
-            studentButton.textContent = obj.students[i].name; // Knapptext visar elevens namn
-    
-            studentButton.addEventListener("click", displayStudentResult.bind(null, studentInfo)); // Anropar funktion vid knapptryck
-    
-            var avgGazeDir = document.createElement('p'); // Text för blickriktningsvärde
-            var avgEyelid = document.createElement('p'); // Text för ögonlockvärde
-            var timeDiagram = document.createElement('img'); // Bild av diagram
-    
-            timeDiagram.src = obj.students[i].timeGraph; // Får adress till varje elevs diagram
-            timeDiagram.style.width = "600px";
-            timeDiagram.style.height = "400px";
-            avgGazeDir.className = "textValues"; // Tilldelas klassen 'textValues'
-            avgEyelid.className = "textValues"; // Tilldelas klassen 'textValues'
-            avgGazeDir.innerHTML = `Genomsnittlig tid med ouppmärksam blickriktning: ${obj.students[i].avgGazeDirection} sekunder`;
-            avgEyelid.innerHTML = `Genomsnittlig tid med ögonlocken stängda: ${obj.students[i].avgEyelid} sekunder`;
-    
-            // Bifogar barn till dess föräldrar
-            studentInfo.appendChild(avgGazeDir);  
-            studentInfo.appendChild(avgEyelid);
-            studentInfo.append(timeDiagram);
-            frag.appendChild(studentButton); 
-            frag.appendChild(studentInfo);   
-        }
-        container.appendChild(frag);
-    },
-    displayHiddenDivFunc: function displayStudentResult(hiddenDiv) { // Togglar fram och tillbaka elevens information
-        if(hiddenDiv.style.display === "none") 
-            hiddenDiv.style.display = "block"; // Visar elevens info om det är osynligt från början
-        else 
-            hiddenDiv.style.display = "none";
-    }
-};*/
