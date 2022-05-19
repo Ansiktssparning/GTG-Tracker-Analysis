3fd5e52b3affd57b44781f0a37821a48c987795f
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2022-05-19 08:50:37 +0200

    Images appear on website again. Also deleted data management because it complicates things, 04:or will deal with it

--- public/tracker_plugin.js
+++ public/tracker_plugin.js
@@ -1,17 +1,3 @@
-/*var net = require('net')
-var JsonSocket = require('json-socket');
-
-var port = 8080; //The same port that the server is listening on
-var host = '127.0.0.1';
-var socket = new JsonSocket(new net.Socket()); //Decorate a standard net.Socket with JsonSocket
-socket.connect(port, host);
-socket.on('connect', function() { //Don't send until we're connected
-    socket.sendMessage({a: 5, b: 7});
-    socket.on('message', function(message) {
-        console.log('The result is: '+message.result);
-    });
-});*/
-
 fetch('http://localhost:3000/students') // API som skickar nätverk requests
     .then(response => response.json())
     .catch(error => console.log('Error:', error)) // Visar error om det fuckar upp
